19927066f54c9aa4f3611b7992b0b5543739632d
Matias Jacob <matias@binagora.com>
2018-08-29 09:51:20 -0300

    merge result command added

---
+++ SqlExplorer.Program/MergeResult.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace SqlExplorer.Program{
+
+    /// Merge result class definition
+    public class MergeResult {
+
+        /// Complete word searched
+        public string WordSearched { get; set; }
+
+        /// List of class information where the word was found
+        public IList<ClassInfo> Found { get; set; }
+    }
+}
---
+++ SqlExplorer.Program/MergeResultCommand.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SqlExplorer.Program
+{
+    /// Merge Result Command class definition
+    public class MergeResultCommand
+    {
+        // Given a file and a pattern it finds all the ocurrences of this word into this file
+        public IList<MergeResult> Execute(IList<SearchResult> input)
+        {
+            var items = new Dictionary<string, IList<SearchResult>>();
+            var result = new List<MergeResult>();
+            foreach (var item in input)
+            {
+                if (!items.ContainsKey(item.WordSearched))
+                    items.Add(item.WordSearched, new List<SearchResult>());
+
+                items[item.WordSearched].Add(item);
+            }
+
+            foreach (var item in items)
+            {
+                result.Add(new MergeResult()
+                {
+                    WordSearched = item.Key,
+                    Found = GetClassFound(item.Value)
+                }
+                );
+            }
+            return result;
+        }
+
+        private IList<ClassInfo> GetClassFound(IList<SearchResult> input)
+        {
+            var result = new List<ClassInfo>();
+
+            foreach (var item in input)
+            {
+                result.Add(new ClassInfo()
+                {
+                    LineNumber = item.LineNumber,
+                    ClassName = item.ClassName
+                });
+            }
+
+            return result;
+        }
+    }
+}
